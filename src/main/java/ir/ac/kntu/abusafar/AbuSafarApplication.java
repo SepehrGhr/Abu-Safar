97e7d040e95aa1b12e9b0bd572da87d992a7c5b7
Sepehr <you@example.com>
2025-03-15 03:56:16 +0330

    Initial commit

---
+++ src/main/java/ir/ac/kntu/abusafar/AbuSafarApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu.abusafar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbuSafarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbuSafarApplication.class, args);
+	}
+
+}
