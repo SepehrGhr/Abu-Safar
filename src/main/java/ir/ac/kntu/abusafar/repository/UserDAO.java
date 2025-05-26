92288b6aeb91fb08f69015aff5c6bfd397efd76d
Sepehr <you@example.com>
2025-05-26 20:25:47 +0430

    feat(UserDAOImpl): implement UserDAO

---
+++ src/main/java/ir/ac/kntu/abusafar/repository/UserDAO.java
@@ -0,0 +1,33 @@
+package ir.ac.kntu.abusafar.repository;
+
+import ir.ac.kntu.abusafar.model.User;
+import ir.ac.kntu.abusafar.model.UserContact;
+import ir.ac.kntu.abusafar.util.constants.enums.ContactType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDAO {
+
+    User save(User user);
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    List<User> findAll();
+
+    int update(User user);
+
+    int deleteById(Long id);
+
+    void saveContact(UserContact userContact);
+
+    List<UserContact> findContactByUserId(Long userId);
+
+    Optional<UserContact> findContactByUserIdAndType(Long userId, ContactType contactType);
+
+    int deleteContact(Long userId, ContactType contactType);
+}
