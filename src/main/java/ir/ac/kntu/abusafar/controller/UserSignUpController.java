e1c2313d8713d1480c4da24c15a111f9a61c3816
Sepehr <you@example.com>
2025-05-27 19:49:42 +0430

    feat(Routes.java): remove hardcodes accross the app

--- src/main/java/ir/ac/kntu/abusafar/controller/UserSignUpController.java
+++ src/main/java/ir/ac/kntu/abusafar/controller/UserSignUpController.java
@@ -3,11 +3,10 @@ package ir.ac.kntu.abusafar.controller;
 import ir.ac.kntu.abusafar.dto.User.SignUpRequestDTO;
 import ir.ac.kntu.abusafar.dto.User.UserInfoDTO;
 import ir.ac.kntu.abusafar.dto.response.BaseResponse;
-import ir.ac.kntu.abusafar.model.User;
 import ir.ac.kntu.abusafar.service.UserService;
+import ir.ac.kntu.abusafar.util.constants.Routes;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping(Routes.API_AUTH)
 public class UserSignUpController {
 
     private final UserService userService;
@@ -25,24 +24,8 @@ public class UserSignUpController {
         this.userService = userService;
     }
 
-    @PostMapping("/signup")
+    @PostMapping(Routes.SIGN_UP)
     public ResponseEntity<BaseResponse<UserInfoDTO>> signUpUser(@Valid @RequestBody SignUpRequestDTO signUpRequest) {
         return ResponseEntity.status(201).body(BaseResponse.success(userService.signUp(signUpRequest)));
     }
-
-//    private UserInfoDTO convertToUserResponse(User user) {
-//        UserResponse response = new UserResponse();
-//        response.setId(user.getId());
-//        response.setFirstName(user.getFirstName());
-//        response.setLastName(user.getLastName());
-//        response.setCity(user.getCity());
-//        response.setUserType(user.getUserType().name());
-//        // We don't want to send email/phone here unless specifically requested,
-//        // or retrieve them via userDAO.findContactByUserId if needed.
-//        // For now, keeping it simple.
-//        if (user.getSignUpDate() != null) {
-//            response.setSignUpDate(user.getSignUpDate().format(DateTimeFormatter.ISO_LOCAL_DATE));
-//        }
-//        return response;
-//    }
 }
---
+++ src/main/java/ir/ac/kntu/abusafar/util/constants/Routes.java
@@ -0,0 +1,12 @@
+package ir.ac.kntu.abusafar.util.constants;
+
+public class Routes {
+
+    public static final String API_KEY = "/api";
+
+    public static final String AUTH = "/auth";
+
+    public static final String API_AUTH = API_KEY + AUTH;
+
+    public static final String SIGN_UP = "/signup";
+}
