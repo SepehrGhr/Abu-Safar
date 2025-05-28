814d39bc91c1f1e58b579a857d566aa454b4068c
Sepehr <you@example.com>
2025-05-28 15:42:20 +0430

    feat(JwtAuthenticationFilter.java): add JwtAuthenticationFilter

--- src/main/java/ir/ac/kntu/abusafar/config/SecurityConfig.java
+++ src/main/java/ir/ac/kntu/abusafar/config/SecurityConfig.java
@@ -1,15 +1,24 @@
 package ir.ac.kntu.abusafar.config;
 
+import ir.ac.kntu.abusafar.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -18,15 +27,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/api/auth/signup", "/api/auth/login/otp/request", "/api/auth/login/otp/verify").permitAll()
-                                .anyRequest().authenticated()
+                        .requestMatchers("/api/auth/signup", "/api/auth/login/otp/request", "/api/auth/login/otp/verify").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable);
-        // .formLogin(Customizer.withDefaults()) // Example: enables form login
-        // .httpBasic(Customizer.withDefaults()); // Example: enables HTTP Basic authentication
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        // .formLogin(Customizer.withDefaults())
+        // .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
