3e7a9af4f2d2808d222611a415a32ad770fae066
SepehrGhr <sepehr.ghardashi@gmail.com>
2025-06-12 04:27:09 +0430

    feat: add cache layer for tickets

--- src/main/java/ir/ac/kntu/abusafar/dto/ticket/TicketSelectRequestDTO.java
+++ src/main/java/ir/ac/kntu/abusafar/dto/ticket/TicketSelectRequestDTO.java
@@ -2,11 +2,15 @@ package ir.ac.kntu.abusafar.dto.ticket;
 
 import ir.ac.kntu.abusafar.util.constants.enums.AgeRange;
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@EqualsAndHashCode
+@ToString
 public class TicketSelectRequestDTO {
     @NotNull(message = "TripId cannot be null.")
     private Long tripId;
